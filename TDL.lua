235699bdc8180d0cbc79ebd55907783ddb9284c8
KJ <xiankai@gmail.com>
2013-02-10 02:23:43 +0800

    On Init Code

--- TDL.lua
+++ TDL.lua
@@ -1,19 +1,48 @@
-local TDL = LibStub("AceAddon-3.0"):NewAddon("TDL")
-
+local TDL = LibStub("AceAddon-3.0"):NewAddon("TDL", "AceConsole-3.0", "AceDB-3.0", "AceEvent-3.0", "AceTimer-3.0-3.0", "AceGUI-3.0-3.0-3.0")
+local realm_defaults = {
+	server_reset = 400
+	weekly_reset = 3 -- Most shit resets on Tuesday past midnight (wed)
+	raid_reset = 4 -- Raid resets on Wednesday past midnight (thu)
+}
+	
 function TDL:OnInitialize()
 -- do init tasks here, like loading the Saved Variables, 
 -- or setting up slash commands.
+
+	-- Realm settings
+	self.db.realm = LibStub("AceDB-3.0"):New("TDL-DB", realm_defaults)
 
 -- init the addon options window
 -- run startMonitoring()
 end
 
 function TDL:OnEnable()
--- so far not needed?
-
 -- Do more initialization here, that really enables the use of your addon.
 -- Register Events, Hook functions, Create Frames, Get information from 
 -- the game that wasn't available in OnInitialize
+
+-- Initialize settings now that we have access to certain variables.
+	-- Character-specific settings
+	local playerName = UnitName("player")
+	local playerObject = self.db.profile.playerName
+	local timers = playerObject.timers
+	-- Profile exists
+	if 	playerObject ~= nil then
+		-- Empty table or doesn't exist
+		if next(timers) == nil then
+			playerObject = {}
+		-- We have a table, mens!
+		else 
+			for k,v in pairs(timers) do
+				if timers.done == false then
+					-- Create timer OR undone message
+					-- Register the complete handlers
+				else
+					-- Create completed message
+				end
+			end
+		end
+	end
 end
 
 function TDL:OnDisable()
@@ -22,15 +51,6 @@ function TDL:OnDisable()
 -- Unhook, Unregister Events, Hide frames that you created.
 -- You would probably only use an OnDisable if you want to 
 -- build a "standby" mode, or be able to toggle modules on/off.
-end
-
-function startMonitoring
--- setup things to monitor based on settings
-	-- read from DB
-	-- check what timers to enable
-	-- print timers
-	-- register event handlers for timer completion {On Boss Kill/On Quest Complete}
-	-- return
 end
 
 function finishMonitoring
@@ -58,4 +78,14 @@ end
 
 function addMonitorUI
 -- add a new row
+end
+
+-- Incomplete, Work on this next time
+function calculateTimeLeft(timetype, timevalue)
+	local monitored_time, current_time = time()
+	local monitored_timetable = date("*t")
+	
+	if timetype == "weekly" then
+		return monitored_timetable.wday 
+	elseif timetype == "daily" then
 end
